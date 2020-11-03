85ce6f88bca260abb2e354a8bc52e392cf7bae20
Mark Cain <Mark.Cain@elekta.com>
2020-11-03 06:53:17 -0600

    Module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -9,6 +10,27 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+
+            var averageGrades = (from s in Students select s.AverageGrade).OrderByDescending(c => c).ToList();
+            var rank = averageGrades.FindIndex(x => Math.Abs(x - averageGrade) < 0.001) + 1;
+            var percentile = (double)rank / Students.Count;
+
+            if (percentile <= .2)
+                return 'A';
+            else if (percentile <= .4)
+                return 'B';
+            else if (percentile <= .6)
+                return 'C';
+            else if (percentile <= .8)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
